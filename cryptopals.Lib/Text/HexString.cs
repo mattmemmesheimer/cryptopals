80aca58354ebb530e00315fcff2e3ddbfabfe93a
Matt Memmesheimer <m.memmesheimer@gmail.com>
2017-09-08 21:25:29 -0500

    Adding Lib projectt and HexString class.

---
+++ cryptopals.Lib/Text/HexString.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cryptopals.Lib.Text
+{
+    public class HexString
+    {
+        #region Properties
+
+        public IReadOnlyList<byte> Bytes { get; }
+
+        #endregion
+
+        public HexString(string hex)
+        {
+            if (hex == null)
+            {
+                throw new ArgumentNullException(nameof(hex));
+            }
+            if (hex.Length % 2 != 0)
+            {
+                throw new ArgumentException(nameof(hex));
+            }
+            Bytes = Enumerable.Range(0, hex.Length).Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16)).ToList();
+        }
+
+        public HexString(IEnumerable<byte> bytes)
+        {
+            Bytes = bytes.ToList();
+        }
+
+        public override string ToString()
+        {
+            if (Bytes.Count == 0)
+            {
+                return string.Empty;
+            }
+            var hex = new StringBuilder(Bytes.Count * 2);
+            foreach (byte b in Bytes)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+            return hex.ToString();
+        }
+    }
+}
