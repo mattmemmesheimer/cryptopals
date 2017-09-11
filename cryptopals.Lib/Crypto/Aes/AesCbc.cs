c39e13161f58d804c7886094da366c9026049097
Matt Memmesheimer <m.memmesheimer@gmail.com>
2017-09-10 21:55:52 -0500

    Completeing challenge 10.

---
+++ cryptopals.Lib/Crypto/Aes/AesCbc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using cryptopals.Lib.Extensions;
+
+namespace cryptopals.Lib.Crypto.Aes
+{
+    public static class AesCbc
+    {
+        private static readonly int BlockSizeBytes = 16;
+
+        public static byte[] Encrypt(byte[] key, byte[] iv, byte[] data)
+        {
+            int paddingLength = BlockSizeBytes - (data.Length % BlockSizeBytes);
+            paddingLength = paddingLength == 0 ? BlockSizeBytes : paddingLength;
+            int cipherTextLength = data.Length + paddingLength;
+            var cipherText = new byte[cipherTextLength];
+            var paddedData = PaddingUtil.Pad(data, BlockSizeBytes);
+            var blocks = paddedData.Batch(BlockSizeBytes);
+            int index = 0;
+            var previousBlock = iv;
+            foreach (var block in blocks)
+            {
+                var xord = XorUtil.Xor(block.ToArray(), previousBlock);
+                var encrypted = AesEcb.Encrypt(key, xord);
+                Array.Copy(encrypted, 0, cipherText, index, BlockSizeBytes);
+                previousBlock = encrypted;
+                index += BlockSizeBytes;
+            }
+            return cipherText;
+        }
+
+        public static byte[] Decrypt(byte[] key, byte[] iv, byte[] data)
+        {
+            var blocks = data.Batch(BlockSizeBytes);
+            var clearText = new byte[data.Length];
+            int index = 0;
+            var previousBlock = iv;
+            foreach (var block in blocks)
+            {
+                var blockArray = block.ToArray();
+                var decrypted = AesEcb.Decrypt(key, blockArray);
+                var xord = XorUtil.Xor(decrypted, previousBlock);
+                Array.Copy(xord, 0, clearText, index, BlockSizeBytes);
+                previousBlock = blockArray;
+                index += BlockSizeBytes;
+            }
+            return PaddingUtil.RemovePad(clearText);
+        }
+    }
+}
---
+++ cryptopals.Test/Challenges/Set2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using cryptopals.Lib.Crypto.Aes;
+using cryptopals.Lib.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cryptopals.Test.Challenges
+{
+    [TestClass]
+    public class Set2
+    {
+        [TestMethod]
+        public void TestChallenge10()
+        {
+            var keyStr = "YELLOW SUBMARINE";
+            var key = System.Text.Encoding.ASCII.GetBytes(keyStr);
+            var iv = new HexString("0000000000000000");
+            var data = Convert.FromBase64String(Set2Data.Challenge10Input);
+
+            var decrypted = AesCbc.Decrypt(key, iv.Bytes.ToArray(), data);
+            var actual = System.Text.Encoding.ASCII.GetString(decrypted);
+
+            Assert.AreEqual(Set2Data.Challenge10Solution, actual);
+        }
+    }
+}
