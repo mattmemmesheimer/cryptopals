3577d106edb4c2c0cf3fd49f89fcf2c49f8e48b5
Matt Memmesheimer <m.memmesheimer@gmail.com>
2017-09-17 16:32:29 -0500

    Adding KVP parser and tests.

---
+++ cryptopals.Lib/Text/KvpParser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cryptopals.Lib.Text
+{
+    public static class KvpParser
+    {
+        public static Dictionary<string, string> Decode(string encoded)
+        {
+            if (encoded == null)
+            {
+                throw new ArgumentNullException(nameof(encoded));
+            }
+            var result = new Dictionary<string, string>();
+            var split = encoded.Split(new[] {'&'}, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var kvp in split)
+            {
+                var s = kvp.Split(new []{'='}, StringSplitOptions.None);
+                if (s.Length != 2 || string.IsNullOrWhiteSpace(s[0]) || s[0] == string.Empty)
+                {
+                    throw new FormatException("Invalid format.");
+                }
+                var key = s[0];
+                var value = s[1];
+                if (result.ContainsKey(key))
+                {
+                    throw new ArgumentException("An element with the same key already exists.");
+                }
+                result[key] = value;
+            }
+            return result;
+        }
+
+        public static string Encode(Dictionary<string, string> input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+            if (input.Count == 0)
+            {
+                return string.Empty;
+            }
+            var sb = new StringBuilder();
+            foreach (var kvp in input)
+            {
+                var key = kvp.Key.Replace("&", string.Empty).Replace("=", string.Empty);
+                var value = kvp.Value.Replace("&", string.Empty).Replace("=", string.Empty);
+                sb.Append($"{key}={value}&");
+            }
+            sb.Remove(sb.Length - 1, 1);
+            return sb.ToString();
+        }
+    }
+}
---
+++ cryptopals.Test/Text/KvpParserTest.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using cryptopals.Lib.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cryptopals.Test.Text
+{
+    [TestClass]
+    public class KvpParserTest
+    {
+        [TestMethod]
+        public void TestDecodeEmpty()
+        {
+            var encoded = "";
+            var result = KvpParser.Decode(encoded);
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void TestDecodeEmptyValue()
+        {
+            var encoded = "a=&b=";
+            var result = KvpParser.Decode(encoded);
+
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(string.Empty, result["a"]);
+            Assert.AreEqual(string.Empty, result["b"]);
+        }
+
+        [ExpectedException(typeof(FormatException))]
+        [TestMethod]
+        public void TestDecodeInvalidFormatEmptyKey()
+        {
+            var encoded = "=";
+            KvpParser.Decode(encoded);
+        }
+
+        [ExpectedException(typeof(FormatException))]
+        [TestMethod]
+        public void TestDecodeInvalidFormatWhitespaceKey()
+        {
+            var encoded = " =";
+            KvpParser.Decode(encoded);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void TestDecodeNull()
+        {
+            KvpParser.Decode(null);
+        }
+
+        [TestMethod]
+        public void TestDecodeSingle()
+        {
+            var encoded = "key=val";
+            var result = KvpParser.Decode(encoded);
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(result["key"], "val");
+        }
+
+        [TestMethod]
+        public void TestDecodeMultiple()
+        {
+            var encoded = "key1=val1&key2=val2&key3=val3";
+            var result = KvpParser.Decode(encoded);
+
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(result["key1"], "val1");
+            Assert.AreEqual(result["key2"], "val2");
+            Assert.AreEqual(result["key3"], "val3");
+        }
+
+        [TestMethod]
+        public void TestEncodeSingle()
+        {
+            var obj = new Dictionary<string, string>
+            {
+                { "key", "value" }
+            };
+
+            var encoded = KvpParser.Encode(obj);
+
+            Assert.AreEqual("key=value", encoded);
+        }
+
+        [TestMethod]
+        public void TestEncodeMultiple()
+        {
+            var obj = new Dictionary<string, string>
+            {
+                { "key1", "val1" },
+                { "key2", "val2" },
+                { "key3", "val3" }
+            };
+
+            var encoded = KvpParser.Encode(obj);
+
+            Assert.AreEqual("key1=val1&key2=val2&key3=val3", encoded);
+        }
+
+        [TestMethod]
+        public void TestEncodeEmpty()
+        {
+            var obj = new Dictionary<string, string>();
+
+            var encoded = KvpParser.Encode(obj);
+
+            Assert.AreEqual(string.Empty, encoded);
+        }
+
+        [TestMethod]
+        public void TestEncodeRemoveInvalidChars()
+        {
+            var obj = new Dictionary<string, string>
+            {
+                { "key1=", "=val1" },
+                { "key2&", "val2&" }
+            };
+
+            var encoded = KvpParser.Encode(obj);
+
+            Assert.AreEqual("key1=val1&key2=val2", encoded);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void TestEncodeNull()
+        {
+            KvpParser.Encode(null);
+        }
+    }
+}
